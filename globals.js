42ffb683657bfc4966f370ee9f9a3cadd349edb2
Philip Read <Philip.Read.000@gmail.com>
2015-10-19 15:54:05 -0500

    Added initial foundation for project.

---
+++ globals.js
@@ -0,0 +1,6 @@
+//This file contains all the namespaces and global variable declarations
+//This file is intended to be used as an index for finding common variables
+
+//namespaces
+
+//global vars
